e674f5d6f348af2ead9cbdd5b49c488a1eadda68
Fred12300 <fredcharpentier2@gmail.com>
2024-07-25 09:04:06 +0200

    Fonctionnel enfin

---
+++ script.js
@@ -0,0 +1,162 @@
+let taskName = document.querySelector("#taskName").value;
+let taskIn = document.querySelector("#taskIn").value;
+let okBtn = document.querySelector("#subBtn");
+let suppr = document.querySelectorAll(".del");
+let dateAff = document.querySelector("#date");
+
+let ref = 3;
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+let day = date.getDate();
+let now = `${day} / ${month} / ${year}`
+
+
+let data = [];
+
+function refresh() {
+    console.log("---Refresh---");
+    console.log(data);
+    document.querySelectorAll(".task").forEach(x => x.remove());
+
+    
+    dateAff.textContent =  `${now}`
+
+    data.map((ele) => {
+        let task = document.createElement("div");
+        zone.appendChild(task);
+        task.style.width = "400px";
+        ele.state === 0 ? task.style.backgroundColor = "rgb(239, 241, 90)" : task.style.backgroundColor = "rgb(95, 241, 90)";
+        task.className = `${data.indexOf(ele)} task`;
+        task.innerHTML = `
+        <div class="line">
+            <h3>Nom : </h3><p class="${data.indexOf(ele)} mod name">${ele.name}<p>
+        </div>
+        <div class="line">
+            <h3>Description : </h3> <p class="${data.indexOf(ele)} mod content">${ele.content}</p>
+        </div>
+        <div class="line"><h5>Date de création : </h5><p class="${data.indexOf(ele)} mod date">${ele.date}</p></div>
+        <hr>
+        <div class="btns" id="b${data.indexOf(ele)}">
+            <button id="${data.indexOf(ele)}" class="del btn" >Supprimer</button>
+            <button class="${data.indexOf(ele)} check btn">Fait</button>
+            <button class="${data.indexOf(ele)} change btn">Modifier</button>
+        </div>`;
+    })
+    suppr = document.querySelectorAll(".del");
+        suppr.forEach(x => x.addEventListener("click", () => {
+            console.log("suppr");
+   
+            deleteT(x.id)
+        }))
+    check = document.querySelectorAll(".check");
+    check.forEach(x => x.addEventListener("click", () => {
+        console.log("ckeck");
+        console.log(x.className[0])
+        data[x.className[0]].state === 0 ? data[x.className[0]].state = 1 : data[x.className[0]].state = 0;
+        refresh()
+    }))
+    change = document.querySelectorAll(".change");
+    change.forEach(x => x.addEventListener("click", () => {
+        console.log("change");
+        changeIt(x.className[0]);
+    }))
+    localStorage.setItem("DataFred", data)
+}
+
+refresh();
+
+function addTask() {
+    console.log("---AddTask---");
+    taskName = document.querySelector("#taskName").value;
+    taskIn = document.querySelector("#taskIn").value;
+    if(taskName === "" || taskIn === "") {
+        alert("Veuillez saisir un Nom ET une Description")
+    } else {
+        let newT = {}
+        newT.name = taskName;
+        newT.content = taskIn;
+        newT.ref = ref;
+        newT.state = 0;
+        newT.date = now;
+        data.push(newT);
+        ref++;
+        refresh()
+    }
+}
+
+function deleteT(nb) {
+    data.splice(nb, 1);
+    refresh();
+}
+
+function changeIt(wich) {
+    let mod = document.getElementsByClassName(`${wich} mod`);
+    console.log(mod);
+    Array.from(mod).map((x) => {
+        console.log(x)
+        let inp = document.createElement("input");
+        inp.classList = `${x.classList}`
+        let nam = x.classList[2];
+        inp.placeholder = data[x.className[0]][`${nam}`];
+        console.log("-------------------------------")
+        console.log(nam)
+        console.log(data[x.className[0]][`${nam}`])
+        x.parentNode.appendChild(inp);
+        x.remove()
+    }
+    );
+    console.log(wich)
+    let btnZone = document.getElementById(`b${wich}`);
+    let btns = document.getElementsByClassName("btn");
+    Array.from(btns).forEach(x => x.remove());
+    let modBtnOk = document.createElement("button");
+    modBtnOk.textContent = "Sauver";
+    modBtnOk.id = "save";
+    
+    let modBtnCancel = document.createElement("button");
+    modBtnCancel.textContent = "Annuler";
+    modBtnCancel.id = "cancel";
+    
+
+    btnZone.appendChild(modBtnOk)
+    btnZone.appendChild(modBtnCancel)
+
+    let saveIt = document.getElementById("save");
+    let cancelIt = document.getElementById("cancel");
+
+    saveIt.addEventListener("click", () => {
+        console.log("---Save---");
+        Array.from(mod).map((x) => {
+        console.log(x.value)
+        if(x.value != "") {
+            //-----------------------------------------------let nam = x.classList[2];
+            let nam = x.classList[2];
+            console.log("SAVE " + x.value)
+            data[wich][`${nam}`] = x.value;
+            refresh()
+        }
+        });
+    });
+    cancelIt.addEventListener("click", () =>  {refresh()});
+}
+
+
+// let deleteT = (nb) => {
+
+// }
+
+okBtn.addEventListener("click", () => {
+    console.log("clic")
+    addTask()
+})
+console.log("DATA" + localStorage.getItem("DataFred", data)) 
+
+
+
+
+
+
+
+
+
